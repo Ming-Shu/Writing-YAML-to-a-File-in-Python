ef8305ee2673ff30ee91267befa6b671f5d1ec88
Ming-Shu Wu <f16cdf16cd@gmail.com>
2021-10-15 14:03:53 +0800

    Add files via upload

---
+++ TBoutputYaml.py
@@ -0,0 +1,47 @@
+
+import sys
+import ruamel.yaml
+
+yaml = ruamel.yaml.YAML()
+yaml.indent(mapping=2,sequence=4,offset=2)
+yaml.compact_seq_map = False
+
+_IP = input('Input your IP：')
+
+myDict = {
+    'thingsboard': {
+        'host': _IP,
+        'port': 1883,
+        'remoteConfiguration': False,
+        'security': {
+            'accessToken': 'MYTOKEN'
+        },
+    },
+    'storage': 
+    {
+        'type': 'memory',
+        'read_records_count': 10,
+        'max_records_count': 100000,
+    }
+}
+with open("tb_gateway_1.yaml", "w") as f:
+    #yaml.dump(myDict, f,default_flow_style = False,sort_keys=False)
+    yaml.dump(myDict,f)
+
+list_connetor = []
+for i in range(1,11):
+
+    data = {'name': 'OPCUA-'+str(i)+'-TEST Connector',
+            'type': 'opcua',
+            'configuration': 'opcua-'+str(i)+'-test.json'}
+            
+    list_connetor.append(data)
+
+
+Data = {
+    'connectors': list_connetor
+}
+
+with open("tb_gateway_1.yaml", "a") as f:
+    #yaml.safe_dump(Data, f, default_flow_style = False,sort_keys=False)
+    yaml.dump(Data,f)
